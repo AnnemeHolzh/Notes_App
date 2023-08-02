14d8823c07d041c5960a671d6eac1185c0220c94
Anneme Holzhausen <ST10144453@vcconnect.edu.za>
2023-08-02 16:11:38 +0200

    search button is consistent!!!!

--- Notes_App/MainWindow.xaml.cs
+++ Notes_App/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Notes_App
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
