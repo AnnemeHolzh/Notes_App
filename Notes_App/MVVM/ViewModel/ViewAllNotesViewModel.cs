ec8531e955a6ed852abc3ecd82b27664d848f261
Try-Angle1134 <102529964+ST10071737@users.noreply.github.com>
2023-08-02 17:13:52 +0200

    did som optemising

--- Notes_App/MVVM/ViewModel/ViewAllNotesViewModel.cs
+++ Notes_App/MVVM/ViewModel/ViewAllNotesViewModel.cs
@@ -4,11 +4,25 @@ using System.Collections.ObjectModel;
 
 namespace Notes_App.MVVM.ViewModel
 {
+    /// <summary>
+    /// View Model for the ViewAllNotesView
+    /// </summary>
     public class ViewAllNotesViewModel : ObservableObjectsClass
     {
+
+        //___________________________________________________________________________________________________________
+        //__________________________________________Parameters_______________________________________________________
+        //___________________________________________________________________________________________________________
+
+        /// <summary>
+        /// a local list of all the notes stored
+        /// </summary>
         private ObservableCollection<NotesClass> _Notes { get; set; }
         //___________________________________________________________________________________________________________
 
+        /// <summary>
+        /// a local list of all the notes stored
+        /// </summary>
         public ObservableCollection<NotesClass> Notes
         {
             get { return _Notes; }
@@ -20,10 +34,18 @@ namespace Notes_App.MVVM.ViewModel
         }
         //___________________________________________________________________________________________________________
 
+        //___________________________________________________________________________________________________________
+        //__________________________________________Constructors_____________________________________________________
+        //___________________________________________________________________________________________________________
+
+        /// <summary>
+        /// constructor that sets the local list of notes to the global list
+        /// </summary>
         public ViewAllNotesViewModel()
         {
             Notes = new ObservableCollection<NotesClass>(NotesListClass.ReturnNotes());
         }
+        //___________________________________________________________________________________________________________
     }
 }
 //____________________________________EOF_________________________________________________________________________
