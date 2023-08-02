822f3ecec3d3b8620d9fd0f9cadccb6d3a09d567
Anneme Holzhausen <ST10144453@vcconnect.edu.za>
2023-08-02 08:41:29 +0200

    wokring on making the search button funky as well (not going well)

--- Notes_App/UI_Code/RandomColorConverter.cs
+++ Notes_App/UI_Code/RandomColorConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//-----------00000000000ooooooooooo..........Start Of File...........ooooooooooo00000000000-----------//
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -11,7 +12,11 @@ namespace Notes_App.UI_Code
 {
     public class RandomColorConverter : IValueConverter
     {
+        //------------------------------------------Creating a list to store the colour values of the button------------------------------------------//
         private int colorIndex = 0;
+        /// <summary>
+        /// The variable that makes iterating through the given colours possible. 
+        /// </summary>
         private readonly List<string> hoverColors = new List<string>
         {
             "#D53302", // Oraney
@@ -21,10 +26,18 @@ namespace Notes_App.UI_Code
             "#00002B"  // Dark Blue 
             // Add more hex codes of colors as needed
         };
-
+        //------------------------------------------Convert Method------------------------------------------//
+        /// <summary>
+        /// This method iterates through the given colours randomly, converts the hex code from the colours to a SolidColourBrush and returns the randomized colour on the button. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is bool isMouseOver && isMouseOver)
+            if (value is bool isMouseOver && isMouseOver) //This checks whether the mouse is hovering over the button or not. 
             {
                 // Get the next hex code from the list
                 string hexCode = hoverColors[colorIndex];
@@ -32,16 +45,26 @@ namespace Notes_App.UI_Code
 
                 // Convert the hex code to a SolidColorBrush
                 var brush = (SolidColorBrush)new BrushConverter().ConvertFrom(hexCode);
-                return brush;
+                return brush; //Displays the colour on the button
             }
 
             // Return the default color if not hovered
             return new SolidColorBrush(Color.FromRgb(238, 238, 238));
         }
-
+        //------------------------------------------Convert Back Method------------------------------------------//
+        /// <summary>
+        /// This method converts the button colour back to the default colour when the user stops hovernig over the button. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
     }
 }
+//-----------00000000000ooooooooooo..........End Of File...........ooooooooooo00000000000-----------//
