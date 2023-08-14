a20436de9e9bb5ab983c6e31d86e77b3689c688d
Try-Angle1134 <102529964+ST10071737@users.noreply.github.com>
2023-08-14 22:09:54 +0200

    sorted the buttons in the view all view between the 3 containers

--- Notes_App/MVVM/View/ViewAllNotesView.xaml.cs
+++ Notes_App/MVVM/View/ViewAllNotesView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Notes_App.Core;
-using Notes_App.MVVM.Model;
+﻿using Notes_App.MVVM.Model;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
@@ -31,16 +30,44 @@ namespace Notes_App.MVVM.View
             if (buttonContainer1 != null)
             {
                 buttonContainer1.Children.Clear();
+                buttonContainer2.Children.Clear();
+                buttonContainer3.Children.Clear();
             }
-            
+
+            var i = 0;
 
             foreach (var note in notes)
             {
-                Button button = new Button();
-                button.Content = $"{note.GetTitle()}";
-                button.Click += Button_Click;
-                button.Style = (Style)FindResource("AppButtonStyle");
-                buttonContainer1.Children.Add(button);
+                if (i < notes.Count)
+                {
+                    i++;
+                    if (i == 1)
+                    {
+                        Button button1 = new Button();
+                        button1.Content = $"{note.GetTitle()}";
+                        button1.Click += Button_Click;
+                        button1.Style = (Style)FindResource("AppButtonStyle");
+                        buttonContainer1.Children.Add(button1);
+                    }
+                    else if (i == 2)
+                    {
+                        Button button2 = new Button();
+                        button2.Content = $"{note.GetTitle()}";
+                        button2.Click += Button_Click;
+                        button2.Style = (Style)FindResource("AppButtonStyle");
+                        buttonContainer2.Children.Add(button2);
+                    }
+                    else if (i == 3)
+                    {
+                        Button button3 = new Button();
+                        button3.Content = $"{note.GetTitle()}";
+                        button3.Click += Button_Click;
+                        button3.Style = (Style)FindResource("AppButtonStyle");
+                        buttonContainer3.Children.Add(button3);
+                        i = 0;
+                    }
+                }
+
             }
         }
 
